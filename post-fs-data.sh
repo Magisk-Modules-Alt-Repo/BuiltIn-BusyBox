43ac0230d02f0621728139809e3b2f9fdd0037ef
zgfg <51943147+zgfg@users.noreply.github.com>
2022-02-16 09:41:49 +0100

    Add files via upload

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,15 +1,20 @@
 MODDIR=${0%/*}
 
 XBINDIR=/system/xbin
+BBXBINDIR=$MODDIR$XBINDIR
+BBBINDIR=$MODDIR/system/bin
+
 if [ -d $XBINDIR ]
 then
-BBDIR=$MODDIR$XBINDIR
+  BBDIR=$BBXBINDIR
 else
-BBDIR=$BBDIR/system/bin
+  BBDIR=$BBBINDIR
 fi
-
 BBBIN=$BBDIR/busybox
 
+rm -rf $BBXBINDIR
+rm -rf $BBBINDIR
 mkdir -p $BBDIR
+
 cp /data/adb/magisk/busybox $BBBIN
 $BBBIN --install -s $BBDIR
