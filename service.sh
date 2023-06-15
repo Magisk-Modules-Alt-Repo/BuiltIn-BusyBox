20fd990264d3ddf4bab0e2b932a1f98b2fc90506
zgfg <51943147+zgfg@users.noreply.github.com>
2023-06-15 08:49:22 +0200

    Add files via upload

--- service.sh
+++ service.sh
@@ -21,37 +21,37 @@ magisk --path
 BB=busybox
 which $BB
 $BB | head -n 1
-SDIR=$(which $BB | sed "s,/$BB$,,")
-if [ -d $SDIR ]
+BBDIR=$(which $BB | sed "s,/$BB$,,")
+if [ -d $BBDIR ]
 then
-  cd $SDIR
+  cd $BBDIR
   pwd
   ls -la | grep $BB | grep ^lr.x | rev | cut -d ' ' -f 3 | rev
   ls -la | grep $BB | grep ^lr.x | wc -l
+fi
+
+# Log results for ToyBox
+TB=toybox
+which $TB
+$TB --version
+TBDIR=$(which $TB | sed "s,/$TB$,,")
+if [ -d $TBDIR ]
+then
+  cd $TBDIR
+  pwd
+  ls -la | grep $TB | grep ^lr.x | rev | cut -d ' ' -f 3 | rev
+  ls -la | grep $TB | grep ^lr.x | wc -l
 fi
 
 # Log results for ToyBox-Ext
 TBEXT=toybox-ext
 which $TBEXT
 $TBEXT --version
-SDIR=$(which $TBEXT | sed "s,/$TBEXT$,,")
-if [ -d $SDIR ]
+TBEXTDIR=$(which $TBEXT | sed "s,/$TBEXT$,,")
+if [ "$TBEXTDIR" != "$TBDIR" ] && [ -d $TBEXTDIR ]
 then
-  cd $SDIR
+  cd $TBEXTDIR
   pwd
-  ls -la | grep $TBEXT | grep ^lr.x | rev | cut -d ' ' -f 3 | rev
-  ls -la | grep $TBEXT | grep ^lr.x | wc -l
-fi
-
-# Log results for stock ToyBox
-TB=toybox
-which $TB
-$TB --version
-SDIR=$(which $TB | sed "s,/$TB$,,")
-if [ -d $SDIR ]
-then
-  cd $SDIR
-  pwd
-  ls -la | grep $TB | grep -v $TBEXT | grep ^lr.x | rev | cut -d ' ' -f 3 | rev
-  ls -la | grep $TB | grep -v $TBEXT | grep ^lr.x | wc -l
+  ls -la | grep $TB | grep ^lr.x | rev | cut -d ' ' -f 3 | rev
+  ls -la | grep $TB | grep ^lr.x | wc -l
 fi
